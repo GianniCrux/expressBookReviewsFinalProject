be4e364ad31c2006af515af396072c1ab31f9803
GianniCrux <gianni.disti@gmail.com>
2024-06-25 12:54:12 +0200

    initial commit

---
+++ final_project/router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "9780141439518" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "9780141439523"  },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "9780141439512"  },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "9780141439598"  },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "9780141439545"  },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "9780141439564"  },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "9780141439553"  },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "9780141439509"  },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "9780141434564"  },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "97801414233218"  }
+}
+
+module.exports=books;
