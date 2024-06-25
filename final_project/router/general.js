86f9960437628f33f6e15c527a260e5be7f8a1a8
GianniCrux <gianni.disti@gmail.com>
2024-06-25 12:59:30 +0200

    implemented endpoint to get the book review based on the isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,8 +50,17 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  const booksArray = Object.values(books);
+
+  const book = booksArray.find(book => book.isbn === isbn);
+
+  if (book) {
+      res.send(book.reviews);
+  } else {
+      res.status(404).json({ error: "Book not found!"});
+  }
 });
 
 module.exports.general = public_users;
