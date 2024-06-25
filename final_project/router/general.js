5b313719119267a89a2682b37af187d2bb6c3c86
GianniCrux <gianni.disti@gmail.com>
2024-06-25 15:37:47 +0200

    implemented register endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,63 +4,85 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!doesExist (username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login"})
+        } else {
+            return res.status(404).json({ message: "User already exists!"});
+        }
+    }
+    return res.status(404).json({ error: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books, null, 4));
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {   
-  const isbn = req.params.isbn;
+    const isbn = req.params.isbn;
 
-  const booksArray = Object.values(books);
+    const booksArray = Object.values(books);
 
-  let filtered_books = booksArray.filter((books) => books.isbn === isbn);
+    let filtered_books = booksArray.filter((books) => books.isbn === isbn);
 
-  res.send(filtered_books);
+    res.send(filtered_books);
 });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author; 
+    const author = req.params.author; 
 
-  const booksArray = Object.values(books);
-  
-  let filtered_books = booksArray.filter((book) => book.author === author);
-  
-  res.send(filtered_books);
+    const booksArray = Object.values(books);
+    
+    let filtered_books = booksArray.filter((book) => book.author === author);
+    
+    res.send(filtered_books);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title; 
+    const title = req.params.title; 
 
-  const booksArray = Object.values(books);
-  
-  let filtered_books = booksArray.filter((book) => book.title === title);
-  
-  res.send(filtered_books);
+    const booksArray = Object.values(books);
+    
+    let filtered_books = booksArray.filter((book) => book.title === title);
+    
+    res.send(filtered_books);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+    const isbn = req.params.isbn;
 
-  const booksArray = Object.values(books);
+    const booksArray = Object.values(books);
 
-  const book = booksArray.find(book => book.isbn === isbn);
+    const book = booksArray.find(book => book.isbn === isbn);
 
-  if (book) {
-      res.send(book.reviews);
-  } else {
-      res.status(404).json({ error: "Book not found!"});
-  }
+    if (book) {
+        res.send(book.reviews);
+    } else {
+        res.status(404).json({ error: "Book not found!"});
+    }
 });
 
 module.exports.general = public_users;
